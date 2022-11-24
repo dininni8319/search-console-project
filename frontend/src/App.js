ffa0dfb74f7ed3249e3bded5f09ba7512e0bc8aa
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-24 22:17:17 +0100

    salvataggio delle implementazioni di frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,14 +8,6 @@ function App() {
               <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-
-
-
-
-
-
-
-
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
