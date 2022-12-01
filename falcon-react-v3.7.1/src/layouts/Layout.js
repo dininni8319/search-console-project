2334bd17e7e6d82c1a608fc368f7ddcb399b4504
Salvatore Dininni <s.dininni@yahoo.com>
2022-12-01 17:36:46 +0100

    aggiunto template al progetto

---
+++ falcon-react-v3.7.1/src/layouts/Layout.js
@@ -0,0 +1,83 @@
+import React, { useContext, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import is from 'is_js';
+import MainLayout from './MainLayout';
+import SettingsToggle from 'components/settings-panel/SettingsToggle';
+import SettingsPanel from 'components/settings-panel/SettingsPanel';
+import ErrorLayout from './ErrorLayout';
+import { toast, ToastContainer } from 'react-toastify';
+import { CloseButton } from 'components/common/Toast';
+import Error404 from 'components/errors/Error404';
+import Error500 from 'components/errors/Error500';
+import SplitLogin from 'components/authentication/split/Login';
+import SplitLogout from 'components/authentication/split/Logout';
+import SplitRegistration from 'components/authentication/split/Registration';
+import SplitForgetPassword from 'components/authentication/split/ForgetPassword';
+import SplitPasswordReset from 'components/authentication/split/PasswordReset';
+import SplitConfirmMail from 'components/authentication/split/ConfirmMail';
+import SplitLockScreen from 'components/authentication/split/LockScreen';
+import AppContext from 'context/Context';
+
+const Layout = () => {
+  const HTMLClassList = document.getElementsByTagName('html')[0].classList;
+  useContext(AppContext);
+
+  useEffect(() => {
+    if (is.windows()) {
+      HTMLClassList.add('windows');
+    }
+    if (is.chrome()) {
+      HTMLClassList.add('chrome');
+    }
+    if (is.firefox()) {
+      HTMLClassList.add('firefox');
+    }
+  }, [HTMLClassList]);
+
+  return (
+    <>
+      <Routes>
+        {/* <Route path="landing" element={<Landing />} /> */}
+        <Route path="/" element={<SplitLogin />} />
+        <Route element={<ErrorLayout />}>
+          <Route path="errors/404" element={<Error404 />} />
+          <Route path="errors/500" element={<Error500 />} />
+        </Route>
+       
+        <Route path="authentication/split/logout" element={<SplitLogout />} />
+        <Route
+          path="authentication/split/register"
+          element={<SplitRegistration />}
+        />
+        <Route
+          path="authentication/split/forgot-password"
+          element={<SplitForgetPassword />}
+        />
+        <Route
+          path="authentication/split/reset-password"
+          element={<SplitPasswordReset />}
+        />
+        <Route
+          path="authentication/split/confirm-mail"
+          element={<SplitConfirmMail />}
+        />
+        <Route
+          path="authentication/split/lock-screen"
+          element={<SplitLockScreen />}
+        /> 
+
+        {/* <Navigate to="/errors/404" /> */}
+        <Route path="*" element={<Navigate to="/errors/404" replace />} />
+      </Routes>
+      <SettingsToggle />
+      <SettingsPanel />
+      <ToastContainer
+        closeButton={CloseButton}
+        icon={false}
+        position={toast.POSITION.BOTTOM_LEFT}
+      />
+    </>
+  );
+};
+
+export default Layout;
