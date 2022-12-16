96e554aa89c56346581e1ae948d2aeda81a1ffbf
Salvatore Dininni <s.dininni@yahoo.com>
2022-12-16 18:19:56 +0100

    implementazioni per il salvataggio dei dati nel DB

--- backend/app/Actions/SearchConsoleAnalyticsAction.php
+++ backend/app/Actions/SearchConsoleAnalyticsAction.php
@@ -3,41 +3,50 @@
 namespace App\Actions;
 
 use Carbon\Carbon;
+use App\Models\User;
 use App\Models\Project;
 use \Google\Service\Webmasters;
+use App\Jobs\ProcessGoogleAnalitycs;
 
 class SearchConsoleAnalyticsAction 
 {
   public function handle($client, $site, $dateStart = null, $dateEnd = null, $num = null)
   {
+    $userId = auth()->guard('api')->user()->id;
+    
     $service = new Webmasters($client);
     $request = new Webmasters\SearchAnalyticsQueryRequest;
     $request->setStartRow(0);
 
     $dateNow = Carbon::now()->format('Y-m-d');
-    if (intval($num) === 7 || intval($num) === 28) {
-      $twoWeeksBefore = Carbon::now()->subDays($num)->format('Y-m-d');
+   
+    $Months = Carbon::now()->subMonths(16)->format('Y-m-d');
 
-      $request->setStartDate($dateStart ? $dateStart : $twoWeeksBefore);
-      $request->setEndDate($dateEnd ? $dateEnd : $dateNow);
-      $request->setSearchType('web');
-      $request->setRowLimit(5000);
-      $request->setAggregationType('byProperty');// you can also query byPage or auto
-      $request->setDimensions(array('date'));
+    $request->setStartDate($dateStart ? $dateStart : $Months);
+    $request->setEndDate($dateEnd ? $dateEnd : $dateNow);
+    $request->setSearchType('web');
+    $request->setRowLimit(5000);
+    $request->setAggregationType('byProperty');// you can also query byPage or auto
+    $request->setDimensions(array('date'));
       // $request->setDimensions(array('query','date', 'country','device','page'));
-      $query_search = $service->searchanalytics->query('https://'.$site, $request); 
-      $rows = $query_search->getRows();
-  
-      $performance = [
-        "clicks" => 0,
-        "impressions" => 0,
-        "position" => 0,
-        "ctr" => 0,
-      ];
+    $query_search = $service->searchanalytics->query('https://'.$site, $request); 
+    $rows = $query_search->getRows();
+    
+    // $getProjectId = Project::where('user_id', '=', $userId); 
+    // dd($getProjectId, 'testing the login user');
+    ProcessGoogleAnalitycs::dispatch($rows);  
+    
+    $performance = [
+      "clicks" => 0,
+      "impressions" => 0,
+      "position" => 0,
+      "ctr" => 0,
+    ];
   
       $count = count($rows);
   
       foreach ($rows as $key => $value) {
+
         $performance['clicks'] += $value['clicks'];
         $performance['impressions'] += $value['impressions'];
         $performance['position'] += $value['position'];
@@ -53,47 +62,7 @@ class SearchConsoleAnalyticsAction
         'count' => $count,
       ]);
       return $data;
-    } else {
-      $twoWeeksBefore = Carbon::now()->subMonths($num)->format('Y-m-d');
-  
-      $request->setStartDate($dateStart ? $dateStart : $twoWeeksBefore);
-      $request->setEndDate($dateEnd ? $dateEnd : $dateNow);
-      $request->setSearchType('web');
-      $request->setRowLimit(5000);
-      $request->setAggregationType('byProperty');// you can also query byPage or auto
-      $request->setDimensions(array('date'));
-      // $request->setDimensions(array('query','date', 'country','device','page'));
-      $query_search = $service->searchanalytics->query('https://'.$site, $request); 
-      $rows = $query_search->getRows();
-  
-      $performance = [
-        "clicks" => 0,
-        "impressions" => 0,
-        "position" => 0,
-        "ctr" => 0,
-      ];
-  
-      $count = count($rows);
-  
-      foreach ($rows as $key => $value) {
-        $performance['clicks'] += $value['clicks'];
-        $performance['impressions'] += $value['impressions'];
-        $performance['position'] += $value['position']; 
-        $performance['ctr'] += $value['ctr'];
-      }
-  
-      $performance['position'] = $performance['position'] / $count;
-      $performance['ctr'] = ($performance['ctr'] / $count) * 100;
-  
-      $data = collect([
-        'rows' => $rows,
-        'performance' => $performance,
-        'count' => $count,
-      ]);
-      return $data;
-
-    }
-
+    
   }
 
   public function handleSites($client){
--- backend/app/Jobs/ProcessGoogleAnalitycs.php
+++ backend/app/Jobs/ProcessGoogleAnalitycs.php
@@ -2,25 +2,27 @@
 
 namespace App\Jobs;
 
+use App\Models\Analytic;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 
 class ProcessGoogleAnalitycs implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $rows;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($rows)
     {
-        //
+        $this->rows = $rows;
     }
 
     /**
@@ -29,7 +31,19 @@ class ProcessGoogleAnalitycs implements ShouldQueue
      * @return void
      */
     public function handle()
-    {
-        //
+    { 
+        foreach ($this->rows as $row) {
+            
+            Analytic::updateOrCreate([
+                "clicks" => $row[0],
+            ],
+            [
+                "clicks" => $row['clicks'],
+                "impressions" => $row['impressions'],
+                "position" => $row['position'],
+                "ctr" => $row['ctr'],
+                "project_id" => $row[4],
+            ]);
+        }
     }
 }
